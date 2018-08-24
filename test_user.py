399894f773716d9a052d0d7486b342819b246b37
Ras-Kwesi <kwesi.makonnen@gmail.com>
2018-08-24 12:58:24 +0300

    test classes teadown method and set up called

--- test_user.py
+++ test_user.py
@@ -1,2 +1,20 @@
 import unittest
+from user import User
 
+
+class TestUser(unittest.TestCase):
+    '''Test class that defines test cases/procedure
+     for the user class cases'''
+
+    def tearDown(self):
+        '''
+        tearDown method that does clean up after each test case has run.
+        '''
+        User.lockusers = []
+
+
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_user = User("Kwesi","Makonnen","0712345678","kwest@a.com") # create contact object
