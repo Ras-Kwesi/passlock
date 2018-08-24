3e7dbb7972a23f8e79142798fa8c3db04077b7d2
Ras-Kwesi <kwesi.makonnen@gmail.com>
2018-08-24 15:18:42 +0300

    instance variable d=for credentails class stated

---
+++ credentials.py
@@ -0,0 +1,17 @@
+class Credentials :
+    '''
+    This is the class for the passwords of the various websie our user is going to use
+    '''
+
+    credentials_dict = {}
+
+    def __init__(self,account_service,password):
+
+        """
+        definig the instance classes for a specific website/app
+        :param account_service:  The service, IG, FB etc
+        :param password: Password belonging to password
+        """
+
+        self.account_service = account_service
+        self.password = password
