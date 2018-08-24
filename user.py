dcb4ca69cf9ea249c1662309b71f7f1da0421d80
Ras-Kwesi <kwesi.makonnen@gmail.com>
2018-08-24 12:40:43 +0300

    class variable defined

--- user.py
+++ user.py
@@ -2,11 +2,14 @@ class User:
     '''This will serve as our
     class for user instances'''
 
+    lockusers = [] #This is a   class variable as any instance has access to this list
+
     def __init__(self,first_name,last_name,password,email):
 
         '''The init function is used to define object properties
         first_name and last_name take the names, while password is
         a credential for modern day login
+        this are accessed only by the instance, not others
         '''
 
         self.first_name = first_name
